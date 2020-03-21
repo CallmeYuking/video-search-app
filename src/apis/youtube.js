2b53865779816c7a98dd72b108317a25f9437457
Yuking Quan <yuking@Yukings-MBP.attlocal.net>
2020-03-20 19:37:37 -0700

    install axios and export its baseURL and params that need to be used in order to interact with youtube api.

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,14 @@
-const KEY = "AIzaSyAYJ697R0n222F8Qmd-mKzHGcgGJjZOeVU"
+import axios from 'axios';
+
+const KEY = "AIzaSyAYJ697R0n222F8Qmd-mKzHGcgGJjZOeVU";
+
+export default axios.create({
+    baseURL: "https://www.googleapis.com/youtube/v3",
+    params: {
+        part: 'snippet',
+        maxResult: 5,
+        key: KEY,
+    }
+});
+
+
